932f5aa1487d201d6a2654d2269341c75b390b43
Harsh Shah <harshdev624@gmail.com>
2023-08-05 18:03:58 +0530

    Removed Debug Banner

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
